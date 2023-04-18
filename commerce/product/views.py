06fd8fbb2a0ef499f9ad96a961da6a178cc5154a
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2023-04-17 21:39:43 -0600

    adds related products view

--- commerce/product/views.py
+++ commerce/product/views.py
@@ -5,8 +5,9 @@ from .models import Product
 
 def detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-
+    related_products = Product.objects.filter(category=product.category, is_sold=False).exclude(pk=pk)[0:3]
 
     return render(request, 'product/detail.html', {
-        'product': product
+        'product': product,
+        'related_products': related_products
     })
